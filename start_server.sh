eb2a7141574a9e5be96e3672cbfa9d52dca9a51f
pkishorp <pkishorp92@gmail.com>
2023-04-26 14:12:26 +0530

    chnages in yml

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 export MYSQL_ROOT_PASSWORD=root
 export MYSQL_DATABASE=userdb
 export MYSQL_USER=root
-export MYSQL_PASSWORD=Pooja@1992
+export MYSQL_PASSWORD=qwert@123
 export MYSQL_HOST=db
 source venv/bin/activate
 python app/manage.py runserver
